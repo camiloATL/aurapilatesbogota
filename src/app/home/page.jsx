446aea2d56e0e8b896f86172237e3c9603e4b838
CamiloAtl <catorres0610@gmail.com>
2026-03-02 12:01:31 -0500

    Text color

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -10,23 +10,23 @@ export default function Home() {
       <div className="h-24 md:h-28" />
 
       <section id="home" className="min-h-screen px-6 py-16">
-        <h1 className="text-4xl font-bold">Home</h1>
+        <h1 className="text-4xl font-bold text-gray-700">Home</h1>
       </section>
 
       <section id="about" className="min-h-screen px-6 py-16">
-        <h2 className="text-3xl font-bold">Nosotros</h2>
+        <h2 className="text-3xl font-bold text-gray-700">Nosotros</h2>
       </section>
 
       <section id="servicios" className="min-h-screen px-6 py-16">
-        <h2 className="text-3xl font-bold">Servicios</h2>
+        <h2 className="text-3xl font-bold text-gray-700">Servicios</h2>
       </section>
 
       <section id="galeria" className="min-h-screen px-6 py-16">
-        <h2 className="text-3xl font-bold">Galería</h2>
+        <h2 className="text-3xl font-bold text-gray-700">Galería</h2>
       </section>
 
       <section id="contacto" className="min-h-screen px-6 py-16">
-        <h2 className="text-3xl font-bold">Contacto</h2>
+        <h2 className="text-3xl font-bold text-gray-700">Contacto</h2>
       </section>
       <Footer />
     </div>
