3ee74817944929d1d5a9468b8457ce916b396c02
CamiloAtl <catorres0610@gmail.com>
2026-03-02 12:04:08 -0500

    font size

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -13,19 +13,19 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-gray-700">Home</h1>
       </section>
 
-      <section id="about" className="min-h-screen px-6 py-16">
+      <section id="about" className="min-h-screen px-6 py-30">
         <h2 className="text-3xl font-bold text-gray-700">Nosotros</h2>
       </section>
 
-      <section id="servicios" className="min-h-screen px-6 py-16">
+      <section id="servicios" className="min-h-screen px-6 py-30">
         <h2 className="text-3xl font-bold text-gray-700">Servicios</h2>
       </section>
 
-      <section id="galeria" className="min-h-screen px-6 py-16">
+      <section id="galeria" className="min-h-screen px-6 py-30">
         <h2 className="text-3xl font-bold text-gray-700">Galería</h2>
       </section>
 
-      <section id="contacto" className="min-h-screen px-6 py-16">
+      <section id="contacto" className="min-h-screen px-6 py-30">
         <h2 className="text-3xl font-bold text-gray-700">Contacto</h2>
       </section>
       <Footer />
